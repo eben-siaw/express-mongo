9f195e9de86b76cdcf0c6e90de249829a89db03a
eben-siaw <61696999+eben-siaw@users.noreply.github.com>
2021-05-20 07:27:17 +0100

    new commit

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 const express = require("express"); 
 const app = express.Router(); 
-const mongoose = require("mongoose"); 
-
+const PORT = process.env.PORT || 4800;
+const mongoose = require("mongoose");   
 
 const connection = mongoose.connect(process.env.mongoURI, { 
-
+ useNewUrlParser, useCreateIndex: true
+}).then((res) => { 
+    console.log("Mongo Connected")
+}).catch(err => { 
+     console.log(err);
 })
 
-app.post("/add-task", (req, res) => { 
+const task = require("./models/task"); 
 
+
+app.post("/add-task", async (req, res) => { 
+ 
+ try {
+    
+ } catch (error) {
+        
+ }
 
 }); 
 
 
-app.post("/show-task", (req, res) => { 
-
+app.post("/show-task", async (req, res) => { 
+ 
+ try {
+        
+ } catch (error) {
+        
+ }
 
 }); 
 
+app.listen(function() { 
+ console.log( 
+  "Server is running on: " +  PORT); 
+})
